9d317029065052dedad3b7683b413cc5d7df5a77
shofukuhara <sho.fukuhara0213@gmail.com>
2022-07-08 00:05:21 +0900

    add:js追記

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
 const hamburger = document.querySelector(".header__hamburger");
 const list = document.querySelector(".header__list");
+const logo = document.querySelector(".header__logo");
+const line = document.querySelectorAll(".header__hamburger-line");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   list.classList.toggle("active");
+});
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 600) {
+    logo.classList.add("scroll");
+    line.forEach((line) => {
+      line.classList.add("scroll");
+    });
+  } else {
+    logo.classList.remove("scroll");
+    line.forEach((line) => {
+      line.classList.remove("scroll");
+    });
+  }
 });
